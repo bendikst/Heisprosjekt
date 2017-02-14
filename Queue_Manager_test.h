436fb6f3a51d925141b466e83480bd878cd8fc99
aalexjo <alexanjohansen@gmail.com>
2017-02-14 13:13:45 +0100

    la inn kommentar

--- Queue_Manager_test.h
+++ Queue_Manager_test.h
@@ -7,7 +7,7 @@ static int qm_last_floor;
 static int qm_next_direction; //-1, 0 , 1
 static int qm_order_list[];
 
-
+//nå la alex inn en kommentar :PPPPPpppPPPppPPpPP
 void qm_update_floor(int floor);
 void qm_update_queue(int floor, button_type_t button);
 int qm_next_floor(void);
