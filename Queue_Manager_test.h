63af25d3e157948319dc65ed7f867e8f2493fc03
Bendik Standal <bendik.standal@gmail.com>
2017-02-14 12:59:09 +0100

    Tester yo

---
+++ Queue_Manager_test.h
@@ -0,0 +1,15 @@
+//This is a test for the module Queue Manager
+
+#ifndef Queue_Manager_test_h
+#define Queue_Manager_test_h
+
+static int qm_last_floor;
+static int qm_next_direction; //-1, 0 , 1
+static int qm_order_list[];
+
+
+void qm_update_floor(int floor);
+
+
+
+#endif /* Queue_Manager_test_h */
